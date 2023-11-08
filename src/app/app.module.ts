2c9057a201ad8c47d253e7ed270ee3983194c714
AmineLARBI <amine.larbi@external.stellantis.com>
2023-11-08 17:27:51 +0100

    First commit of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TeamGamesComponent } from './team-games/team-games.component';
+
+const routes: Routes = [
+  { path: 'leagues/:name', component: LeaguesComponent },
+  { path: 'leagues/:name/:teamID', component: TeamGamesComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeaguesComponent,
+    TeamGamesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
